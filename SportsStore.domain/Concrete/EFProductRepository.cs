1742ad7eb529334cd63210317464853972801835
Simon <swu.sim@gmail.com>
2017-02-19 23:23:16 -0800

    Add files via upload

---
+++ SportsStore.domain/Concrete/EFProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System.Collections.Generic;
+
+namespace SportsStore.Domain.Concrete
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private EFDbContext context;
+
+        public IEnumerable<Product> Products
+        {
+            get { return context.Products; }
+        }
+        public EFProductRepository()
+        {
+            context = new EFDbContext();
+        }
+    }
+}
